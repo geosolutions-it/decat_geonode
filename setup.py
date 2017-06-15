44af0cb39af36371945317485ce3376f804346d2
allyoucanmap <bovio.stefano@gmail.com>
2017-06-15 13:06:37 +0200

    - DECAT theme: fisrt draft version

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from distutils.core import setup
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name="decat_geonode",
+    version="0.1",
+    author="",
+    author_email="",
+    description="decat_geonode, based on GeoNode",
+    long_description=(read('README.rst')),
+    # Full list of classifiers can be found at:
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+    license="BSD",
+    keywords="decat_geonode geonode django",
+    url='https://github.com/decat_geonode/decat_geonode',
+    packages=['decat_geonode',],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+       'geonode>=2.5',
+    ],
+)
