88ed1d467f6b663ddfd465d5c83eb7553f258bb6
Cezary Statkiewicz <cezio@thelirium.net>
2017-06-29 10:42:29 +0200

    settings for decat

--- decat_geonode/settings.py
+++ decat_geonode/settings.py
@@ -1208,3 +1208,45 @@ RISKS = {'DEFAULT_LOCATION': None,
                            'ARGS': []}}
 
 ADMIN_MODERATE_UPLOADS = False
+
+try:
+    from geonode.local_settings import *
+except ImportError:
+    from geonode.settings import *
+#
+# General Django development settings
+#
+
+SITENAME = 'decat_geonode'
+
+# Defines the directory that contains the settings file as the LOCAL_ROOT
+# It is used for relative settings elsewhere.
+LOCAL_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static_root')
+
+WSGI_APPLICATION = "decat_geonode.wsgi.application"
+
+
+# Load more settings from a file called local_settings.py if it exists
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
+# Additional directories which hold static files
+STATICFILES_DIRS.append(
+    os.path.join(LOCAL_ROOT, "staticfiles"),
+)
+
+# Location of url mappings
+ROOT_URLCONF = 'decat_geonode.urls'
+
+# Location of locale files
+LOCALE_PATHS = (
+    os.path.join(LOCAL_ROOT, '..', 'locale'),
+    ) + LOCALE_PATHS
+
+INSTALLED_APPS = INSTALLED_APPS + ('decat_geonode',)
+
+TEMPLATES[0]['DIRS'].insert(0, os.path.join(LOCAL_ROOT, '..', "templates"))
+
+
