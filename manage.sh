3daa4978a0e23b3cbdc78e399300f0583829b625
Cezary Statkiewicz <cezio@thelirium.net>
2017-09-19 13:44:41 +0200

    manage.sh accept all arguments from cli

--- manage.sh
+++ manage.sh
@@ -4,4 +4,4 @@ source ~/.venvs/geonode/bin/activate
 
 pushd $(dirname $0)
 
-DJANGO_SETTINGS_MODULE=decat_geonode.local_settings python manage.py $1
+DJANGO_SETTINGS_MODULE=decat_geonode.local_settings python manage.py $@
